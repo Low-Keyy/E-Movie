a8aec621a7c845e3c88edb1d34b012ed679f3561
Lucas <872592971@qq.com>
2023-04-15 16:52:07 +0800

    差推荐算法

---
+++ pi/src/main/java/team/pi/demo/config/swaggerConfig.java
@@ -0,0 +1,10 @@
+package team.pi.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableOpenApi
+public class swaggerConfig {
+}
